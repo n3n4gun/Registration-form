bcbda40c10513a686b0dded3be922534a9121ce3
Fedor Naboyshchikov <fedor.naboyshchikov@gmail.com>
2023-06-09 15:53:18 +0300

    # 08.06.2023

--- registr_app.py
+++ registr_app.py
@@ -8,6 +8,8 @@ class RegistrForm(customtkinter.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # defining the appearance of the form
+
         self.title('Registr from')
         self.geometry('400x500')
         self.resizable(False, False)
@@ -29,30 +31,40 @@ class RegistrForm(customtkinter.CTk):
 
     def send_data_server(self):
 
+        # defining the socket for connection to the server
+        # socket will be work with IPv4 and use TCP
+
         application_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # get input data from the user
 
         self.name = self.name_field.get()
         self.surname = self.surname_field.get()
         self.email = self.email_field.get()
         self.password = self.password_field.get()
 
+        # verification of the entered data
+
         if self.name == '':
-            messagebox.showerror('Warnings!', 'Name field is empty!')
+            messagebox.showwarning('Warnings!', 'Name field is empty!')
             self.name_field.focus()
 
         elif self.surname == '':
-            messagebox.showerror('Warnings!', 'Surname field is empty!')
+            messagebox.showwarning('Warnings!', 'Surname field is empty!')
             self.surname_field.focus()
 
         elif self.email == '':
-            messagebox.showerror('Warnings!', 'Email field is empty!')
+            messagebox.showwarning('Warnings!', 'Email field is empty!')
             self.email_field.focus()
 
         elif self.password == '':
-            messagebox.showerror('Warnings!', 'Password field is empty!')
+            messagebox.showwarning('Warnings!', 'Password field is empty!')
             self.password_field.focus()
         
         else:
+
+            # if verification complete we put data in the dictionary (personal_info)
+            # password we will store in the form of a hash (sha256)
 
             person_info = {
                     'name' : self.name,
@@ -62,17 +74,24 @@ class RegistrForm(customtkinter.CTk):
                 }
 
             try:
-                application_socket.connect(('127.0.0.1', 8888))
+                application_socket.connect(('127.0.0.1', 8888)) # we try to get connection with server socket which is bind at localhost (127.0.0.1) at 8.8.8.8 port
 
             except:
-                pass
+                messagebox.showwarning('Warnings', 'Connection with server is failed!') # if connection is failed we show warning
 
             else:
-                send_data = f'{person_info["name"]} : {person_info["surname"]} : {person_info["email"]} : {person_info["password"]}'.encode()
 
-                application_socket.send(send_data)
+                # if connection with server have been established...
+                # we get data from the dictionary and put them in the special string which will be send on the server
+                # string will be encode in utf-8
 
-                application_socket.close()
+                send_data = f'{person_info["name"]} : {person_info["surname"]} : {person_info["email"]} : {person_info["password"]}'.encode('utf-8')
+
+                application_socket.send(send_data) # sending data
+
+                application_socket.close() # after sending connection with server close
+
+                # at the end, the fields are cleared
 
                 self.name_field.delete(0, 'end')
                 self.surname_field.delete(0, 'end')
--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import socket
 
+# server socket will work with IPv4 and use TCP
+# socket will be bind with special addres 127.0.0.1 (localhost) and port 8.8.8.8
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('127.0.0.1', 8888))
 
-server_socket.listen()
+server_socket.listen() 
+
+# in this cycle, we will constantly monitor for connectivity
 
 while True:
     try:
@@ -13,6 +18,9 @@ while True:
         server_socket.close()
     
     else:
+
+        # when the socket gets connected it starts accepting incoming data
+
         recv_data = application_socket.recv(1024).decode('utf-8').split(':')
         application_socket.close()
 
